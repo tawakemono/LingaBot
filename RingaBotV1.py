c32fba0b2181c260a2216b2472794e0f86b97a42
tawakemono <2005ryuno@gmail.com>
2023-02-06 10:23:14 +0900

    feat: add headless mode

--- RingaBotV1.py
+++ RingaBotV1.py
@@ -4,6 +4,9 @@ import chromedriver_binary
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 import time
 
 
@@ -12,7 +15,7 @@ Alist = []
 
 
 
-def login():
+def login(loginid,loginpass):
     try:
         #リンガポルタのページへ行く
         driver.get('https://w5.linguaporta.jp/user/seibido/')
@@ -21,16 +24,17 @@ def login():
         #IDを入力
         element = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/form/table/tbody/tr[1]/td/input")#elementに入力欄のパスを代入
         element.clear()#入力欄の中を削除
-        element.send_keys("ログインID")#自分のログインID
+        element.send_keys(loginid)#自分のログインID
 
         #passを入力
         element = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/form/table/tbody/tr[2]/td/input")
         element.clear()
-        element.send_keys("ログインパスワード")#自分のパスワード
+        element.send_keys(loginpass)#自分のパスワード
 
         #ログインボタンを押す
         element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/form/table/tbody/tr[3]/td/div/input")
-        element.click()#ボタンを押す
+        driver.execute_script('arguments[0].click();', element)#ボタンを押す
+#        print("Can Login")
 
     except:
         print("Cannot login")
@@ -41,14 +45,16 @@ def selectcoset():
         #本の選択画面へ移行
         wait. until(EC.presence_of_all_elements_located)
         element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/dl/dt[2]/form/a")
-        element.click()
+        driver.execute_script('arguments[0].click();', element)
 
         #coset2600を選択
         wait. until(EC.presence_of_all_elements_located)
         element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/form/div/div[2]/div[3]/input")
-        element.click()
+        driver.execute_script('arguments[0].click();', element)
+#        print("Can go cosset page")
 
     except:
+        print("Incorrect password")
         print("Cannot go coset page")
 
 
@@ -74,13 +80,15 @@ def Answer():
             print("question in Qlist")
             #Qlistから問題番号を取得
             i = Qlist.index(question)
-            print(i)
+            a=i
+            print("listnum = "+str(a))
 
             #Alistから答えを取得
             anser = Alist[i]
             anser = str(anser)
             anser = anser.replace(" ","")
-            print(anser)
+            print("question = "+question)
+            print("anser = "+anser)
 
             #答えを入力
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/div/div[3]/div/input")
@@ -90,14 +98,17 @@ def Answer():
             #回答ボタンを押す
             wait. until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/input[3]")))
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/input[3]")
-            element.click()
+            driver.execute_script('arguments[0].click();', element)
 
             #次へ進を押す
             WebDriverWait(driver, 10). until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form/input[1]")))
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form/input[1]")
-            element.click()
+            driver.execute_script('arguments[0].click();', element)
+            print("正解")
 
         else:
+            print("question not in Qlist")
+            print("question = "+question)
             #回答欄に適当な答えを入力
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/div/div[3]/div/input")
             element.clear
@@ -106,13 +117,13 @@ def Answer():
             #回答ボタンを押す
             wait. until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/input[3]")))
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/form/input[3]")
-            element.click()
+            driver.execute_script('arguments[0].click();', element)
 
             #正解を見るを押す
             wait. until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form[1]/input[2]")))
             wait.until(EC.presence_of_all_elements_located)
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form[1]/input[2]")
-            element.click()
+            driver.execute_script('arguments[0].click();', element)
 
             #問題の答えを入手
             wait. until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form/input[1]")))
@@ -123,10 +134,10 @@ def Answer():
 
             #次に進むを押す
             element = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/table/tbody/tr[4]/td/div/form/input[1]")
-            element.click()
+            driver.execute_script('arguments[0].click();', element)
 
-            print(Alist)
-            print(Qlist)
+            #print(Alist)
+            #print(Qlist)
 
         return(0)
 
@@ -153,10 +164,10 @@ def CheckUnitEnd():
 
 
 
-#print("自分のIDを入力してください")
-#loginid = str(input())
-#print("ログインパスワードを入力してください")
-#loginpass = str(input())
+print("自分のIDを入力してください")
+loginid = str(input())
+print("ログインパスワードを入力してください")
+loginpass = str(input())
 
 while(1):
     print("開始する番号を入力して下さい")
@@ -181,21 +192,30 @@ while(1):
     if(Unitnum >= Unitnum_end):
         print("指定されたUnitまでの解答が完了しました")
         break
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print("Now    "+str(Unitnum-24)+"->"+str(Unitnum))
     #Chromeを開く
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-logging')
+    options.add_argument('--log-level=3')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
     #タイムアウトの時間を設定
     wait = WebDriverWait(driver, 20)
-    login()
+    login(loginid,loginpass)
     selectcoset()
     selectUnit(Unitnum)
     a = CheckUnitEnd()
     if(a == 1):
+        print("This unit is already done")
         Unitnum = Unitnum+25
         Qlist.clear()
         Alist.clear()
         driver.close()
         continue
     while(1):
+        print("======================================")
         b = 0
         b = Answer()
         if(b == 1):
